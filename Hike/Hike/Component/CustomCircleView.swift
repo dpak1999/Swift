f5e61e68efeaac8cd532f6956ec0a5c2110de950
deepak <deepakkumardash313@gmail.com>
2025-03-01 17:56:15 +0530

    add : custom motion view

--- Hike/Hike/Component/CustomCircleView.swift
+++ Hike/Hike/Component/CustomCircleView.swift
@@ -11,28 +11,31 @@ struct CustomCircleView: View {
     @State private var isAnimationGradient: Bool = false
     
     var body: some View {
-        Circle()
-            .fill(
-                LinearGradient(
-                    colors: [
-                        .customIndigoMedium,
-                        .customSalmonLight
-                    ],
-                    startPoint: 
-                        isAnimationGradient ? .topLeading : .bottomLeading,
-                    endPoint:
-                        isAnimationGradient ? .bottomTrailing : .topTrailing
+        ZStack {
+            Circle()
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            .customIndigoMedium,
+                            .customSalmonLight
+                        ],
+                        startPoint: 
+                            isAnimationGradient ? .topLeading : .bottomLeading,
+                        endPoint:
+                            isAnimationGradient ? .bottomTrailing : .topTrailing
+                    )
                 )
-            )
-            .onAppear {
-                withAnimation(
-                    .linear(duration: 3.0)
-                    .repeatForever(autoreverses: true)
-                ) {
-                    isAnimationGradient.toggle()
+                .onAppear {
+                    withAnimation(
+                        .linear(duration: 3.0)
+                        .repeatForever(autoreverses: true)
+                    ) {
+                        isAnimationGradient.toggle()
+                    }
                 }
-            }
-            .frame(width: 256, height: 256)
+            MotionAnimationView()
+        }
+        .frame(width: 256, height: 256)
     }
 }
 
