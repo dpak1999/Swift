5a19e7c1ae9ffc495a50eb5362aa8a868232dd1b
deepak <deepakkumardash313@gmail.com>
2025-03-02 02:21:51 +0530

    init wishlist app

---
+++ Wishlist/Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Deepak kumar Dash on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
