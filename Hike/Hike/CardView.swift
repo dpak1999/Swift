3d8a723c7b5bec10dadf77d7b4e785b2bdf1c3fc
deepak <deepakkumardash313@gmail.com>
2025-03-01 13:42:28 +0530

    Add : card header text

--- Hike/Hike/CardView.swift
+++ Hike/Hike/CardView.swift
@@ -12,17 +12,38 @@ struct CardView: View {
         ZStack {
             CustomBackgroundView()
             
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, 
-                            endPoint: .bottomTrailing)
+            VStack {
+                VStack (alignment: .leading) {
+                    Text("Hiking")
+                        .fontWeight(.black)
+                        .font(.system(size: 52))
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [
+                                    .customGrayLight,
+                                    .customGrayMedium
+                                ], startPoint: .top, endPoint: .bottom)
                     )
-                    .frame(width: 256, height: 256)
+                    Text("Fun and enjoyable outdoor activities for friends and families.")
+                        .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                }
+                .padding(.horizontal, 30)
                 
-                Image("image-1")
-                    .resizable()
-                .scaledToFit()
+                
+                ZStack {
+                    Circle()
+                        .fill(
+                            LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, 
+                                endPoint: .bottomTrailing)
+                        )
+                        .frame(width: 256, height: 256)
+                    
+                    Image("image-1")
+                        .resizable()
+                    .scaledToFit()
+                }
             }
         }
         .frame(width: 320, height: 570)
