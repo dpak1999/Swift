7b0b90da5c3af456954080587822a765d89e1c01
deepak <deepakkumardash313@gmail.com>
2025-03-01 17:33:05 +0530

    add : linear animation and custom circle view

---
+++ Hike/Hike/Component/CustomCircleView.swift
@@ -0,0 +1,41 @@
+//
+//  CustomCircleView.swift
+//  Hike
+//
+//  Created by Deepak kumar Dash on 01/03/25.
+//
+
+import SwiftUI
+
+struct CustomCircleView: View {
+    @State private var isAnimationGradient: Bool = false
+    
+    var body: some View {
+        Circle()
+            .fill(
+                LinearGradient(
+                    colors: [
+                        .customIndigoMedium,
+                        .customSalmonLight
+                    ],
+                    startPoint: 
+                        isAnimationGradient ? .topLeading : .bottomLeading,
+                    endPoint:
+                        isAnimationGradient ? .bottomTrailing : .topTrailing
+                )
+            )
+            .onAppear {
+                withAnimation(
+                    .linear(duration: 3.0)
+                    .repeatForever(autoreverses: true)
+                ) {
+                    isAnimationGradient.toggle()
+                }
+            }
+            .frame(width: 256, height: 256)
+    }
+}
+
+#Preview {
+    CustomCircleView()
+}
